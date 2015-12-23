3beda3e4810ceb649a888152047965d19109ee3e
Brett LaBombarda <bplabombarda@gmail.com>
2015-12-22 20:51:27 -0800

    Ignoring test JSON files.

--- tweeter.py
+++ tweeter.py
@@ -1,85 +1,95 @@
+import os
+import json
+import logging
+
 import tweepy
 import requests
-from bs4 import BeautifulSoup
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+logging.basicConfig(filename='error.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s: %(message)s')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-res = requests.get('http://www.sportsnet.ca/hockey/nhl/scores/')
-soup = BeautifulSoup(res.text, 'html.parser')
-# test = open('/Users/brett/Projects/3on3bot/ot_test.html')
-# soup = BeautifulSoup(test, 'html.parser')
+url = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) \
+                            AppleWebKit/537.36 (KHTML, like Gecko) \
+                            Chrome/47.0.2526.106 Safari/537.36'}
+res = requests.get(url, headers=headers)
+data = res.text[15:-1]
+json_data = json.loads(data)
+games = json_data['games']
 
-# Grab all game cards elements from the soup
-game_cards = soup.find_all(class_='game-card-container')
+# =====================================================================
+# Testing shit
+# =====================================================================
+# with open(os.path.join(dir_path, 'ot_SJS.json'), 'r') as data_file:
+#     data = data_file.read()
+#     games = json.loads(data)
 
-
-def parse_game(game_card, api):
-
-    # Game ID
-    game_id = game_card.attrs['id'].split('_')[3]
-    # print(game_id)
-
-    # Game Info
-    game_start = game_card.find(class_='game-time-start')
-    game_status = game_card.find(class_='scores-game-status')
-    game_progress = game_status.find(class_="game-current-time")
-    game_period = game_status.find(class_="game-current-period")
-    period_end = game_status.find(class_='period-end')
-    overtime = game_card.select('.scores-game-status td')
-    game_final = game_card.find(class_='final')
-
-    # Away Team Info
-    away_team_info = game_card.find(class_='team-container-1')
-    away_team_city = away_team_info.text.strip('\n').split('\n')[2]
-    away_team_name = away_team_info.text.strip('\n').split('\n')[3]
-    # away_team_logo = away_team_info.select(
-    #                     '.scores-team-logo img')[0].attrs['src']
-    away_team_score = away_team_info.find(class_='scores-team-score')
-
-    # Home Team Info
-    home_team_info = game_card.find(class_='team-container-2')
-    home_team_city = home_team_info.text.strip('\n').split('\n')[2]
-    home_team_name = home_team_info.text.strip('\n').split('\n')[3]
-    # home_team_logo = home_team_info.select(
-    #                     '.scores-team-logo img')[0].attrs['src']
-    home_team_score = home_team_info.find(class_='scores-team-score')
-
-    if (game_status != None):
-        print(game_status)
-
-        print("period_end")
-
-        # tweet_game(api, overtime, away_team_name, away_team_score,
-        #                 home_team_name, home_team_score)
-
-    elif overtime[0].text.lstrip().split(" ")[1] == 'OT':
-        print('overtime')
-        print(overtime[0].text.lstrip().split(" ")[1])
-
-        # tweet_game(api, overtime, away_team_name, away_team_score,
-        #                 home_team_name, home_team_score)
+# =====================================================================
+# Load hashtag JSON file
+# =====================================================================
+with open(os.path.join(dir_path, 'hashtags.json'), 'r') as ht_file:
+    data = ht_file.read()
+    hashtags = json.loads(data)
 
 
-    # if game_final != None and \
-    #     game_final.text.replace("FINAL (", "").replace(")", "").split(" ")[0] == 'OT':
-    #     print("game_final.text")
-    #     print(game_final.text.replace("FINAL (", "").replace(")", "").split(" ")[0])
+def parse_game(game, hashtags):
+
+    game_prog = game['tsc']
+    game_status = game['bs']
+    game_period = game['ts']
+    away_team_name = game['atv']
+    away_team_score = game['ats']
+    home_team_name = game['htv']
+    home_team_score = game['hts']
+
+    # =================================================================
+    # Find team hashtags
+    # =================================================================
+    for hashtag in hashtags:
+        if hashtag['team_name'] == away_team_name:
+            away_team_ht = hashtag['hashtag']
+            away_team_abbr = hashtag['abbreviation']
+
+        if hashtag['team_name'] == home_team_name:
+            home_team_ht = hashtag['hashtag']
+            home_team_abbr = hashtag['abbreviation']
+
+    # =================================================================
+    # Check END 3RD
+    # =================================================================
+    if game['bs'] == 'LIVE' and game['ts'] == 'END 3rd':
+
+        tweet_game(api, away_team_ht, away_team_score,
+                    home_team_ht, home_team_score)
+
+    # =================================================================
+    # Check OT
+    # =================================================================
+    elif game['bs'] == 'LIVE' and game['ts'].split(" ")[1] == 'OT':
+
+        tweet_game(api, away_team_ht, away_team_score,
+                    home_team_ht, home_team_score)
 
 
-def tweet_game(api, overtime, away_team_name, away_team_score,
-                home_team_name, home_team_score):
+def tweet_game(api, away_team_ht, away_team_score,
+                home_team_ht, home_team_score):
 
-    if home_team_score and away_team_score:
-        score = home_team_score.text.lstrip().strip(" ")
-    # if overtime:
-    status = 'Tied at ' + score + away_team_name + ' @ ' + \
-                home_team_name + ' in #3on3OT'
-    print(status)
-    api.update_status(status=status)
+    if home_team_score == away_team_score:
+        score = home_team_score
+        status = away_team_ht + ' @ ' + home_team_ht + \
+                    ' tied at ' + score + ' in #3on3OT'
+        # print(status)
+        api.update_status(status=status)
 
+    else:
+        pass
 
-# Loop over game cards
-for card in game_cards:
-    parse_game(card, api)
+for game in games:
+    parse_game(game, hashtags)
